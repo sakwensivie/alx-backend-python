382817b0e31d79082b0e7a0975cddbe5f057708f
Samuel Akwensivie <sakwnesivie@gmail.com>
2023-05-02 03:32:13 +0000

    saving the file

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''Multiple execution of wait_random'''
+
+
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''Waits for a random number of seconds'''
+    list_float = []
+    for i in range(n):
+        list_float.append(await wait_random(max_delay))
+    return sorted(list_float)
